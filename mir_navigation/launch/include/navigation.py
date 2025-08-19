2a0c1a3ce532dbc0425e5027a0226be161c93712
Oscar Lima <oscar.lima@dfki.de>
2025-08-19 23:19:20 +0200

    various fixes that make mir navigation work in jazzy

--- mir_navigation/launch/include/navigation.py
+++ mir_navigation/launch/include/navigation.py
@@ -114,8 +114,8 @@ def generate_launch_description():
                 remappings=remappings,
             ),
             Node(
-                package='nav2_recoveries',
-                executable='recoveries_server',
+                package='nav2_behaviors',
+                executable='behavior_server',
                 name='recoveries_server',
                 output='screen',
                 parameters=[configured_params],
